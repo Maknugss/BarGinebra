eb7e88deb14e336334b7066994fdb9970408b09a
Cristián Ramírez <cristian.566.ra@hotmail.com>
2023-02-18 12:31:16 -0500

    Se agrega la base del proyecto inicial

---
+++ src/main/java/com/BarGinebra/BarGinebraApplication.java
@@ -0,0 +1,13 @@
+package com.BarGinebra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarGinebraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarGinebraApplication.class, args);
+	}
+
+}
